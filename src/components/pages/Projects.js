400a75e19e4797ecc2af04cae96330fabf026cb1
jmauwerb <jeroen.werbrouck@ugent.be>
2021-12-16 14:48:27 +0100

    jeroen

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -12,18 +12,18 @@ export default function Projects() {
         <table className="table">
             <tbody>
              <tr>
-                <th style={{verticalAlign: "top"}}>
-                <div className="project-info">Project: Pluswonen</div>
-                <div className="project-info">Architects: Arne De Ridder and Maarten Lauwereys</div>
-                <div className="project-info">Under the supervision of: Trice and Tilke</div>
-                <div className="project-info">Course: Architectuurontwerp III</div>
-                <div className="project-info">Year: 3rd bachelor civil engineering and architecture (2020)</div>
+                <th className="project-info">
+                <div>Project: Pluswonen</div>
+                <div>Architects: Arne De Ridder and Maarten Lauwereys</div>
+                <div>Under the supervision of: Trice and Tilke</div>
+                <div>Course: Architectuurontwerp III</div>
+                <div>Year: 3rd bachelor civil engineering and architecture (2020)</div>
                 </th>
-                <th>
-                    <div className="project-description">The concept of spatial efficiency is the starting point for a design study into new organisations/forms of living beyond the classic single-family dwelling with a careful and integrated elaboration.</div>
-                    <div className="project-description">The assignment is to design an innovative house with increased spatial efficiency on a classic plot in the city. In the context of this assignment, intensification, interpretability and interweaving are particularly relevant parameters for achieving an increased return. In concrete terms, this means that we start from the housing needs of an average family (parent with two children). We are asked to provide space that can be flexibly filled according to changing circumstances and needs. This can range from grandparents living at home to students, a change in family composition (children leaving home, own practice, co-working), or the desire to temporarily accommodate one or more people on the basis of a social commitment.</div>
-                    <div className="project-description">Increasing the spatial efficiency should increase the quality of life as much as possible and certainly not affect it. Therefore, it is not a matter of simply adding extra surface area to a traditional family home, but of researching an intelligent linking/interchangeability/flexibility whereby the transitions/separations/access can be differentiated according to the filling in.</div>
-                    <div className="project-description">The aim of this design is to integrate all parameters into an intelligent and spatial design with a high experiential and visual value. Since it is about living, it is about the experience of domesticity.</div>
+                <th className="project-description">
+                    <div >The concept of spatial efficiency is the starting point for a design study into new organisations/forms of living beyond the classic single-family dwelling with a careful and integrated elaboration.</div>
+                    <div>The assignment is to design an innovative house with increased spatial efficiency on a classic plot in the city. In the context of this assignment, intensification, interpretability and interweaving are particularly relevant parameters for achieving an increased return. In concrete terms, this means that we start from the housing needs of an average family (parent with two children). We are asked to provide space that can be flexibly filled according to changing circumstances and needs. This can range from grandparents living at home to students, a change in family composition (children leaving home, own practice, co-working), or the desire to temporarily accommodate one or more people on the basis of a social commitment.</div>
+                    <div>Increasing the spatial efficiency should increase the quality of life as much as possible and certainly not affect it. Therefore, it is not a matter of simply adding extra surface area to a traditional family home, but of researching an intelligent linking/interchangeability/flexibility whereby the transitions/separations/access can be differentiated according to the filling in.</div>
+                    <div>The aim of this design is to integrate all parameters into an intelligent and spatial design with a high experiential and visual value. Since it is about living, it is about the experience of domesticity.</div>
                 </th>
              </tr>
             </tbody>
